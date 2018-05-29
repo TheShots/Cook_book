b7d49d42c1cafa1a64a6823cbdf1704d11d72fd7
TheShots <vancouver28@abv.bg>
2018-05-30 00:25:30 +0300

    Application base

---
+++ Cook/app/src/main/java/com/example/martin28/cook/Spisuk.java
@@ -0,0 +1,16 @@
+package com.example.martin28.cook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Martin28 on 7.5.2016 г..
+ */
+public class Spisuk extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spisuk);
+    }
+}
